cc6f3fe0ad8a61de1c21e7553124aa643fbfd137
andiataleb <111662670+andiataleb@users.noreply.github.com>
2022-12-12 12:42:03 -0800

    Add files via upload

--- MecheCarChallenge.R
+++ MecheCarChallenge.R
@@ -1,8 +1,17 @@
-> library(dplyr) #load the dplyr package
-> MechaCar <-read.csv('MechaCar.csv', check.names = F, stringsAsFactors = F) #Importing and rading the csv file
-> lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar) #Perform Linear Regression using the lm() function
-> summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)) #
-# Summary for p-value and r-squared value for the linear regression model
-> 
+#Deliverable 1
+> library(dplyr) #importing the library
+> MechaCar <- read.csv('MechaCar_mpg.csv',check.names=F, stringsAsFactors = F) #import and read the csv file.
+> lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar) # Linear Regression using the lm() function 
+> summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar)) #determining the p-value and the r-squared value for the linear regression model
 
+# Deliverable 2
+> suspension_coil <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F) #Importing the suspension_coil.csv file
+> total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #Finding the summary ifo for the PSI column
+> View(total_summary)
+> lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #Finding the summary info based on groupby
+
+#Part 3
+t.test(x=subset(suspension_coil,Manufacturing_Lot == "Lot1")$PSI,mu=1500) #t-test for lot 1's PSI
+t.test(x=subset(suspension_coil,Manufacturing_Lot == "Lot2")$PSI,mu=1500) #t-test for Lot 2's PSI
+t.test(x=subset(suspension_coil,Manufacturing_Lot == "Lot3")$PSI,mu=1500) #t-test for Lot 3's PSI
 
